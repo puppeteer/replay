4c0cf6ad4873a2696d6a8618fd18b30304d50f43
Alex Rudenko <OrKoN@users.noreply.github.com>
2022-06-09 15:22:26 +0200

    docs: add extension example (#173)

---
+++ examples/chrome-extension/DevToolsPlugin.js
@@ -0,0 +1,15 @@
+export class RecorderPlugin {
+  async stringify(recording) {
+    return JSON.stringify(recording, null, 2);
+  }
+  async stringifyStep(step) {
+    return JSON.stringify(step, null, 2);
+  }
+}
+
+/* eslint-disable no-undef */
+chrome.devtools.recorder.registerRecorderExtensionPlugin(
+  new RecorderPlugin(),
+  /* name=*/ 'CustomJSON',
+  /* mediaType=*/ 'application/json'
+);

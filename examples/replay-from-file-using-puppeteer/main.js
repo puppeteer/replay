c6f6116bb9c691e4b94aee7f3b50f8df56f33161
Abiiget765 <abiiget765@gmail.com>
2025-06-16 17:23:50 +0300

    Update main.js
    import { createRunner, parse } from '@puppeteer/replay';
    import fs from 'fs';
    
    // Read recording for a file.
    const recordingText = fs.readFileSync('./recording.json', 'utf8');
    // Validate & parse the file.
    const recording = parse(JSON.parse(recordingText));
    // Create a runner and execute the script.
    const runner = await createRunner(recording);
    await runner.run();

--- examples/replay-from-file-using-puppeteer/main.js
+++ examples/replay-from-file-using-puppeteer/main.js
@@ -8,3 +8,13 @@ const recording = parse(JSON.parse(recordingText));
 // Create a runner and execute the script.
 const runner = await createRunner(recording);
 await runner.run();
+import { createRunner, parse } from '@puppeteer/replay';
+import fs from 'fs';
+
+// Read recording for a file.
+const recordingText = fs.readFileSync('./recording.json', 'utf8');
+// Validate & parse the file.
+const recording = parse(JSON.parse(recordingText));
+// Create a runner and execute the script.
+const runner = await createRunner(recording);
+await runner.run();
